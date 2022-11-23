b3863fc999ece077cd451f16e35b672e2ee448de
Ekaterina Belova <kbelovaa17@gmail.com>
2022-11-23 16:14:19 +0300

    style: connected font style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from 'Store';
 import App from 'Components/App/App';
+import 'Assets/fonts/fonts.scss';
 import './index.scss';
 
 const rootElement = document.getElementById('root');
